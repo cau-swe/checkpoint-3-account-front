b5fdd4ab7688fa11e188ce6f20eec23fe4643225
MinseungSeon <seonminseung@naver.com>
2021-05-31 01:33:54 +0900

    [upload] admin home page VC 생성

---
+++ pizzaAdministratorApp/AdminPageHomeViewController.swift
@@ -0,0 +1,16 @@
+//
+//  AdminPageHomeViewController.swift
+//  pizzaAdministratorApp
+//
+//  Created by 선민승 on 2021/05/31.
+//
+
+import UIKit
+
+class AdminPageHomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
