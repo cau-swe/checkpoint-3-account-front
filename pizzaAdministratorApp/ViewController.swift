0eb913fcf738e86ea2741dd0e629a35f430617a8
MinseungSeon <seonminseung@naver.com>
2021-05-31 01:24:37 +0900

    [upload] xcodeproj 생성

---
+++ pizzaAdministratorApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  pizzaAdministratorApp
+//
+//  Created by 선민승 on 2021/05/30.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
