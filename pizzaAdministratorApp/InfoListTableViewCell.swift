b1bc16e0563014d2db6f2fbdb9c04f94094ec5f2
MinseungSeon <seonminseung@naver.com>
2021-05-31 20:34:21 +0900

    [feat] table view delegate 연결, cell VC 생성 후 연결

---
+++ pizzaAdministratorApp/InfoListTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  InfoListTableViewCell.swift
+//  pizzaAdministratorApp
+//
+//  Created by 선민승 on 2021/05/31.
+//
+
+import UIKit
+
+class InfoListTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
--- pizzaAdministratorApp/InfoListViewController.swift
+++ pizzaAdministratorApp/InfoListViewController.swift
@@ -9,11 +9,31 @@ import UIKit
 
 class InfoListViewController: UIViewController {
 
+    @IBOutlet var infoListTableView: UITableView!
     @IBOutlet var headerLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        infoListTableView.dataSource = self
     }
     
 
+}
+
+extension InfoListViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        14
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "InfoListTableViewCell") as? GeneralTableViewCell else {
+            return UITableViewCell()
+        }
+        cell.selectionStyle = .none
+        cell.pizzaImage.image = UIImage(named: imageList[indexPath.row])
+        cell.pizzaName.text = menuList[indexPath.row]
+        cell.pizzaDetail.text = menuDetail[indexPath.row]
+        return cell
+        
+    }
 }
