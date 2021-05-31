b3e0e1c7ad2e853c8d0468d8bb0b587222c1cf0d
MinseungSeon <seonminseung@naver.com>
2021-05-31 10:56:31 +0900

    [feat] 관리 home 버튼 IBAction 연결 완료

--- pizzaAdministratorApp/AdminPageHomeViewController.swift
+++ pizzaAdministratorApp/AdminPageHomeViewController.swift
@@ -9,6 +9,15 @@ import UIKit
 
 class AdminPageHomeViewController: UIViewController {
 
+    @IBAction func menuAdminPageButtonPressed(_ sender: Any) {
+    }
+    @IBAction func employeeAdminPageButtonPressed(_ sender: Any) {
+    }
+    @IBAction func CustomerAdminPageButtonPressed(_ sender: Any) {
+    }
+    @IBAction func orderAdminPageButtonPressed(_ sender: Any) {
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
