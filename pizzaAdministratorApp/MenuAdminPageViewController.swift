01b5c806bdb16b8e66794925bbdcc559319f32fc
MinseungSeon <seonminseung@naver.com>
2021-05-31 11:39:33 +0900

    [feat] menu 정보 table view delegate 코딩 완료

--- pizzaAdministratorApp/MenuAdminPageViewController.swift
+++ pizzaAdministratorApp/MenuAdminPageViewController.swift
@@ -7,13 +7,34 @@
 
 import UIKit
 
-class MenuAdminPageViewController: UIViewController {
+class MenuAdminPageViewController: UIViewController{
 
+    let menuList:[String] = ["치즈 피자", "베이컨 피자", "치킨 피자", "페퍼로니 피자", "포테이토 피자"]
+    
     @IBOutlet var menuTableView: UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        menuTableView.dataSource = self
+    }
+    
+}
+
+extension MenuAdminPageViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        4
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "GeneralTableViewCell") as? GeneralTableViewCell else {
+            return UITableViewCell()
+        }
+        cell.selectionStyle = .none
+
+        return cell
 
     }
+    
+    
     
 }
