de3f1c26c8c916246f967b14c277fed9e7042ce9
MinseungSeon <seonminseung@naver.com>
2021-05-31 13:38:10 +0900

    [feat] 피자메뉴 뷰 홈에서 연결

--- pizzaAdministratorApp/AdminPageHomeViewController.swift
+++ pizzaAdministratorApp/AdminPageHomeViewController.swift
@@ -10,7 +10,13 @@ import UIKit
 class AdminPageHomeViewController: UIViewController {
 
     @IBAction func menuAdminPageButtonPressed(_ sender: Any) {
+        guard let menuAdminPageViewController
+ = self.storyboard?.instantiateViewController(identifier: "MenuAdminPageViewController") as? MenuAdminPageViewController else {
+            return
+        }
         
+        // 다음 뷰로 연결
+        self.navigationController?.pushViewController(        menuAdminPageViewController, animated: true)
     }
     @IBAction func employeeAdminPageButtonPressed(_ sender: Any) {
         
