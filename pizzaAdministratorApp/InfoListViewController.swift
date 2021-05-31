4ae1eaebd3da0c1ee8de70c9a62ac38a3a84c44f
MinseungSeon <seonminseung@naver.com>
2021-05-31 20:53:31 +0900

    [feat] tableview 의 내용이 직원정보, 고객정보, 주문기록에 따라 다른 정보로 보여지도록 처리해줌

--- pizzaAdministratorApp/InfoListViewController.swift
+++ pizzaAdministratorApp/InfoListViewController.swift
@@ -9,17 +9,38 @@ import UIKit
 
 class InfoListViewController: UIViewController {
 
+    var listCategory: Int?
     var customerInfoList: [String] = ["선민승","조재경", "김승아", "최윤아", "김컴공", "김피자", "최부자", "김멋사", "최자랑", "최사랑", "김꽃", "김갑부", "미란이", "죠르디"]
     var orderInfoList: [String] = ["포테이토 피자 2판", "페퍼로니 피자 10판", "포테이토 피자 1판", "파인애플 피자 40판", "포테이토 피자 10판", "핫소스 피자 1판", "페퍼로니 피자 10판", "포테이토 피자 1판", "파인애플 피자 40판", "포테이토 피자 10판" ]
     var employeeInfoList: [String] = [ "김피자", "최부자", "김멋사", "최자랑", "최사랑", "김꽃", "김갑부", "미란이", "죠르디","선민승","조재경", "김승아", "최윤아", "김컴공"]
 
+    var list: [String] = []
     
     @IBOutlet var infoListTableView: UITableView!
     @IBOutlet var headerLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        if let category = listCategory {
+            switch category {
+            case 1: // 직원정보
+                headerLabel.text = "직원 정보"
+                list = employeeInfoList
+
+            case 2: // 고객 정보
+                headerLabel.text = "고객 정보"
+                list = customerInfoList
+
+            case 3: // 주문 기록
+                headerLabel.text = "주문 기록"
+                list = orderInfoList
+            default:
+                return
+            }
+        }
         infoListTableView.dataSource = self
+        infoListTableView.reloadData()
+
     }
     
 
@@ -27,7 +48,7 @@ class InfoListViewController: UIViewController {
 
 extension InfoListViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        14
+        list.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
@@ -35,7 +56,8 @@ extension InfoListViewController: UITableViewDataSource {
             return UITableViewCell()
         }
         cell.selectionStyle = .none
-        
+        cell.number.text = String(indexPath.row)
+        cell.name.text = list[indexPath.row]
         return cell
         
     }
